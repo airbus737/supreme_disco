e73c300877df921610fd417774512d0461de2d01
airbus737 <airbus737@js>
2021-05-10 15:07:14 -0500

    organizing

---
+++ python/courses/test/loops.py
@@ -0,0 +1,2 @@
+for num in range(10,89,2):
+    print(num)
---
+++ python/courses/test/luca.py
@@ -0,0 +1,13 @@
+import os
+
+name = input("Hello, what is your name? ")
+
+feeling = input("How are you " + name + "? ")
+
+print("That is great " + name + ". I am glad you are " + feeling + "!!")
+
+.0.
+
+# def speak(name, feeling):
+
+# 	os.system("say goodbye" %name)
