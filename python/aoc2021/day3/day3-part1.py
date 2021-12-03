c995243a7ee0d2e891a1029c0d108db5bf169d2b
Josh S <airbus737@gmail.com>
2021-12-03 15:03:59 -0600

    part1

--- python/aoc2021/day3/day3-part1.py
+++ python/aoc2021/day3/day3-part1.py
@@ -1,11 +1,22 @@
-from collections import Counter
+from collections import defaultdict
 
-aoc_input = open("day3-input.txt").readlines()
+with open('day3-input.txt', 'r') as input:
+    data = [str(d.strip()) for d in input.readlines()]
 
-from collections import Counter
- 
-def most_frequent(aoc_input):
-    occurence_count = Counter(aoc_input)
-    return occurence_count.most_common(1)[0][0]
-   
-print(most_frequent(aoc_input))
+results = defaultdict(list)
+
+for d in data:
+    for index, value in enumerate(d):
+        results[index].append(value)
+
+gamma, epsilon = "", ""
+
+for bit, values in results.items():
+    if values.count("0") > values.count("1"):
+        gamma += "0"
+        epsilon += "1"
+    else:
+        gamma += "1"
+        epsilon += "0"
+
+print(f'G: {gamma} E: {epsilon} Power: {int(gamma, 2) * int(epsilon, 2)}')
