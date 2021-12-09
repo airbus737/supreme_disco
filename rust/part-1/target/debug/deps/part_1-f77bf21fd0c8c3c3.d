0bf7734ab23f702cdff4a9a6b0a27bd573a05529
Josh S <31864258+airbus737@users.noreply.github.com>
2021-12-09 12:13:01 -0600

    starting rust stuff

---
+++ rust/part-1/target/debug/deps/part_1-f77bf21fd0c8c3c3.d
@@ -0,0 +1,5 @@
+/home/airbus737/Documents/GitHub/supreme_disco/rust/part-1/target/debug/deps/part_1-f77bf21fd0c8c3c3: src/main.rs
+
+/home/airbus737/Documents/GitHub/supreme_disco/rust/part-1/target/debug/deps/part_1-f77bf21fd0c8c3c3.d: src/main.rs
+
+src/main.rs:
---
+++ rust/part-1/target/debug/part-1.d
@@ -0,0 +1,1 @@
+/home/airbus737/Documents/GitHub/supreme_disco/rust/part-1/target/debug/part-1: /home/airbus737/Documents/GitHub/supreme_disco/rust/part-1/src/main.rs
