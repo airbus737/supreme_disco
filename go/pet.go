1b783243ccbace3644eebe439e9a60e62422cb7a
Josh S <31864258+airbus737@users.noreply.github.com>
2021-12-10 01:05:30 -0600

    starting go

---
+++ go/pet.go
@@ -0,0 +1,12 @@
+package main
+
+import "fmt"
+import "time"
+
+func main () {
+  fmt.Println("  __      _")
+  fmt.Println("o'')}____//")
+  fmt.Println(" `_/      )")
+  fmt.Println(" (_(_/-(_/ ")
+  fmt.Println(time.Now())
+}
