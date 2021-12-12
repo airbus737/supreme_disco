a2c123cd027797a597a353a848f97dbce0dc2038
Josh S <31864258+airbus737@users.noreply.github.com>
2021-12-12 00:23:05 -0600

    comic mischief

--- go/main.go
+++ go/main.go
@@ -1,11 +1,27 @@
 package main
 
-import (
-  "fmt"
-  t "time"
-)
+import "fmt"
 
 func main() {
-	fmt.Println("Hello World")
-  fmt.Println(t.Now())
+  floatExample := 1.75
+  // Edit the following Printf for the FIRST step
+  fmt.Printf("Working with a ____", floatExample) 
+  
+  fmt.Println("\n***") // Added for spacing
+  
+  yearsOfExp := 3
+  reqYearsExp := 15
+  // Edit the following Printf for the SECOND step
+  fmt.Printf("I have ...", yearsOfExp, reqYearsExp) 
+  
+  fmt.Println("\n***") // Added for spacing
+  
+  stockPrice := 3.50
+  // Edit the following Printf for the THIRD step
+  fmt.Printf("", stockPrice) 
 }
+
+
+
+
+
